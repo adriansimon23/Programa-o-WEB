431d79d72486c89108784463893fb941b0006178
miguel <guegosilvestre67@gmail.com>
2023-05-01 20:57:41 -0300

    arrundo para terminar

--- script.js
+++ script.js
@@ -1,37 +1,31 @@
-const palavras = [
-	{ palavra: "banana", dica: "fruta" },
-	{ palavra: "abacaxi", dica: "fruta" },
-	{ palavra: "macarrão", dica: "comida" },
-    { palavra: "Bola", dica: "futebol"},
-    { palavra: "Alemanha", dica: "países"}
-];
+const socket = new WebSocket('ws://localhost:8080');
 
-const palavraAtual = palavras[Math.floor(Math.random() * palavras.length)];
+socket.addEventListener('open', () => {
+  console.log('Conexão estabelecida com o servidor');
+});
 
-const tamanhoPalavra = palavraAtual.palavra.length;
+socket.addEventListener('message', (event) => {
+  const message = JSON.parse(event.data);
+  if (message.type === 'dica') {
+    document.querySelector('#dica').textContent = message.dica;
+    document.querySelector('#tamanho').textContent = message.tamanho;
+    document.querySelector('#tentativas').textContent = message.tentativas;
+    document.querySelector('#letra').disabled = false;
+    document.querySelector('#letra').focus();
+  } else if (message.type === 'palavra') {
+    document.querySelector('#palavra').textContent = message.palavra;
+    document.querySelector('#tentativas').textContent = message.tentativas;
+    document.querySelector('#letra').value = '';
+    document.querySelector('#letra').focus();
+  } else if (message.type === 'gameover') {
+    alert(`Fim de jogo! A palavra era ${message.palavra}.`);
+    document.querySelector('#guess-form').style.display = 'none';
+  }
+});
 
-const tentativasRestantes = tamanhoPalavra + 3;
-
-let palavraAdivinhada = [];
-
-for (let i = 0; i < tamanhoPalavra; i++) {
-	palavraAdivinhada.push("_");
-}
-
-const palavraElement = document.getElementById("palavra");
-palavraElement.innerHTML = palavraAdivinhada.join(" ");
-
-const dicaElement = document.getElementById("dica");
-dicaElement.innerHTML = palavraAtual.dica;
-
-const tamanhoElement = document.getElementById("tamanho");
-tamanhoElement.innerHTML = tamanhoPalavra;
-
-const tentativasElement = document.getElementById("tentativas");
-tentativasElement.innerHTML = tentativasRestantes;
-
-const formulario = document.querySelector("form");
-formulario.addEventListener("submit", function(event) {
-	event.preventDefault();
-	const letraElement = document.getElementById("letra");
-})
+document.querySelector('#guess-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const letra = document.querySelector('#letra').value;
+  socket.send(letra);
+  document.querySelector('#letra').disabled = true;
+});
--- servidorWeb.js
+++ servidorWeb.js
@@ -1,57 +1,73 @@
-// rodando Node Js no LocalHost
-
-const http = require('http');
 const express = require('express');
-// const WebSocket = require('ws');
-
-const app = express();
-const server = http.createServer(app);
-// const ws = new WebSocket.Server({ server });
-
-const {
-    MongoClient
-} = require('mongodb');
+const { MongoClient } = require('mongodb');
+const fs = require('fs/promises');
+const WebSocket = require('ws');
 
 var db;
 var locais;
-var names;
-async function conecta()
-{
-    var client = new MongoClient('mongodb://localhost:27017');
-    await client.connect();
-    db = await client.db("NOVO_DB");
-    locais = await db.collection("places");
-    // Inicia o servidor
-    server.listen(4000, () => {
-    console.log('Servidor iniciado na porta 27');
+var wss;
+
+async function conecta() {
+  var client = new MongoClient('mongodb://127.0.0.1:27017');
+  await client.connect();
+  db = await client.db("NOVO_DB");
+  locais = await db.collection("places");
+}
+
+async function inserir(req, res) {
+  try {
+    const local = req.body;
+    let a = await locais.insertOne(local);
+    console.log(a);
+    res.send('Nova localização adicionada');
+    wss.clients.forEach(client => {
+      client.send('Nova localização adicionada');
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Erro ao adicionar localização');
+  }
+}
+
+async function listar(req, res) {
+  try {
+    let a = await locais.find({}).toArray();
+    res.send(a);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Erro ao listar localizações');
+  }
+}
+
+async function remover(req, res) {
+  try {
+    const nome = req.params.nome;
+    let a = await locais.deleteOne({ nome: nome });
+    console.log(a);
+    res.send('Localização removida com sucesso');
+  } catch (e) {
+    console.log(e);
+    res.status(500).send('Erro ao remover localização');
+  }
+}
+
+async function init() {
+  await conecta();
+  const app = express();
+  app.use(express.json());
+  app.post('/locais', inserir);
+  app.get('/locais', listar);
+  app.delete('/locais/:nome', remover);
+  app.listen(3000, () => {
+    console.log('Servidor iniciado na porta 3000');
   });
-}
-
-async function inserte()
-{
-    try {
-        await locais.insertOne(JSON.stringify(msg));
-        
-    } catch (e) {
-        print (e);
-    }
-}
-
-async function lista()
-{
-    try {
-        const palavras = await locais.getCollectionInfos();
-        print(palavras)
-    } catch (error) {
-        print ("error");
-    }
-}
-
-
-async function init()
-{
-    await conecta();
-    await inserte();
+  wss = new WebSocket.Server({ port: 8080 });
+  wss.on('connection', (ws) => {
+    console.log('Cliente conectado');
+    ws.on('message', (message) => {
+      console.log(`Recebido mensagem: ${message}`);
+    });
+  });
 }
 
 init();
